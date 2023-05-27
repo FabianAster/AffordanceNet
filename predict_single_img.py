added better way to check if dataset is correct, and fixed mask mirroring issue; TODO: scaling function

predict_single_img.py
@@ -11,49 +11,6 @@ import importlib
 from PIL import Image
 
 
-# Adapt data to tf dataset
-def iit_data_to_tf_dataset():
-    for roi in roidb:
-        final_obj = {
-            'image':  roi['image'],     # imagepath
-            # empty variable for future img shape
-            'image_shape': tf.constant([0., 0.], dtype=tf.float32),
-            'objects': {
-                'bbox': tf.constant(roi['boxes'], dtype=tf.float32),
-                'label': tf.constant(roi['gt_classes'], dtype=tf.int32),
-                # keep only mask label
-                'seg_mask_inds': tf.constant(0, dtype=tf.int32),
-                'mask_path': tf.constant(np.array(['s']), dtype=tf.string),
-                'mask':  tf.constant(tf.constant(0.), dtype=tf.float32)
-            }
-        }
-        if cfg.MASK_REG:
-            # Create paths to read masks afterwards
-            num_bboxes = len(roi['boxes'])
-            im_ind = str(roi['seg_mask_inds'][0][0])
-            mask_paths = [os.path.join(cfg.DATA_MASKS_DIR_IMGS, im_ind + '_' + str(i) + '_segmask.png') for i in
-                          range(1, num_bboxes + 1)]
-            final_obj['objects']['seg_mask_inds'] = tf.constant(
-                roi['seg_mask_inds'][:, 1], dtype=tf.int32)    # keep only mask label
-            final_obj['objects']['mask_path'] = tf.constant(
-                np.array(mask_paths), dtype=tf.string)
-        yield final_obj
-
-# Load image and mask data
-
-
-def load_imgs_and_masks(obj_json):
-    if cfg.MASK_REG:
-        masks = tf.map_fn(lambda x: data_utils.read_mask(
-            x), obj_json['objects']['mask_path'], dtype=tf.uint8)
-        obj_json['objects']['mask'] = masks
-
-    # load the raw data from the file as a string and decode image
-    img = tf.io.read_file(obj_json['image'])
-    obj_json['image'] = tf.image.decode_jpeg(img, channels=3)
-    return obj_json
-
-
 if __name__ == '__main__':
     keras.backend.clear_session()
 
@@ -72,6 +29,7 @@ if __name__ == '__main__':
     imdb, roidb = data_utils.combined_roidb(
         cfg.IMDB_NAME, cfg.PROPOSAL_METHOD, cfg.USE_FLIPPED, mode="inference")
     labels = imdb.classes
+    print(labels)
     base_anchors = bbox_utils.generate_base_anchors(cfg)
 
     # Create models and load weights
@@ -99,10 +57,8 @@ if __name__ == '__main__':
     img_mask_check = tf.io.read_file("../data/cache/GTsegmask_VOC_2012_train_images/3736_1_segmask.png")
     tensor_img_mask = tf.io.decode_image(img_mask_check, channels=3, dtype=tf.dtypes.float32)
     np.set_printoptions(threshold=sys.maxsize) 
-    print(tensor_img_mask)
 
     filenames = next(walk("./testRobotImages"), (None, None, []))[2]  # [] if no file
-    print(filenames)
     
     for filename in filenames:
         img = tf.io.read_file("./testRobotImages/"+filename)

trainSetGeneration/draw_bounding_boxes.py
@@ -1,16 +1,23 @@
 import cv2
+import numpy as np
+import os
 import xml.etree.ElementTree as ET
+import matplotlib.pyplot as plt
 
-def draw_bounding_boxes(image_path, xml_path, output_path):
+def draw_bounding_boxes(filename, image_path, xml_path, mask_path):
     # Read image
-    img = cv2.imread(image_path)
+    img = cv2.imread(image_path +"/"+ filename + ".jpg")
+    print("image loaded")
 
     # Parse XML file
-    tree = ET.parse(xml_path)
+    tree = ET.parse(xml_path + "/"+ filename + ".xml")
     root = tree.getroot()
+    print("xml loaded")
 
     # Loop through bounding boxes in XML and draw them on the image
+    counter = 0
     for obj in root.findall('object'):
+        counter +=1
         bbox = obj.find('bndbox')
         xmin = int(bbox.find('xmin').text)
         ymin = int(bbox.find('ymin').text)
@@ -19,8 +26,39 @@ def draw_bounding_boxes(image_path, xml_path, output_path):
 
         # Draw bounding box rectangle on the image
         cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+        
+        # Draw mask on image
+        
+        mask_filename = f"{filename}_{counter}_segmask.png"
+        mask = cv2.imread(mask_path+"/"+mask_filename, 0) 
+        print(mask)
+        # Reshape the grayscale image to have 3 channels
+        mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
 
+        output_image = np.zeros_like(mask)
+        output_image[:, :, 0] = mask[:,:,0] * 100
+
+
+        img = cv2.addWeighted(img, 1, output_image, 0.5, 0)
+        
+        
+
+    
+    
+    
     # Save the image with bounding boxes
-    cv2.imwrite(output_path, img)
+    print("show image")
+    plt.imshow(img)
+    plt.axis('off')  # Turn off the axis labels
+    plt.show()
+    
 
-draw_bounding_boxes("/home/fabian/31_TrainingImages/training_set/imgs_generated/238.jpg", "/home/fabian/31_TrainingImages/training_set/xml_generated/238.xml", "./test1.png")
+mask_dest = '/home/fabian/github/data/cache/GTsegmask_VOC_2012_train_images'
+xml_dest = '/home/fabian/github/data/VOCdevkit2012/VOC2012/Annotations'
+imgs_dest = '/home/fabian/github/data/VOCdevkit2012/VOC2012/JPEGImages'
+
+for filename in os.listdir(imgs_dest):
+    if filename.endswith('.jpg'):
+        filename = filename[:-4]
+        print(filename)
+        draw_bounding_boxes(filename, imgs_dest, xml_dest, mask_dest)